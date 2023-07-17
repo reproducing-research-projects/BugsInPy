504ae7bde1b2deda952581eb6bde77ee81353262
Faustino Aguilar <faustinoaq@gmail.com>
2023-07-16 22:12:58 -0500

    Update txt and add required setup.sh for luigi

---
+++ projects/luigi/bugs/17/setup.sh
@@ -0,0 +1,2 @@
+python setup.py install
+pip install mock==4.0.2 psutil==5.7.0 nose==1.3.7 SQLAlchemy==1.3.13
---
+++ projects/luigi/bugs/19/setup.sh
@@ -0,0 +1,2 @@
+python setup.py install
+pip install mock==4.0.2 psutil==5.7.0 nose==1.3.7
